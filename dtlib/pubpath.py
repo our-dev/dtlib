b986d92ba8ee0ba4f3a70989acb434e7ae545cf7
Harmo <1295351490@qq.com>
2018-09-14 14:52:11 +0800

    commit msg nothing

---
+++ dtlib/pubpath.py
@@ -0,0 +1,8 @@
+"""
+一些公有的文件库
+"""
+from os.path import expanduser
+
+pictures = expanduser("~") + '/Pictures'
+documents = expanduser("~") + '/Documents'
+downloads = expanduser("~") + '/Downloads'
